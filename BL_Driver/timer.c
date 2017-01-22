9317a3621d5c921d0f5420640e224473ede283a6
chibinh8 <chibinh8@gmail.com>
2017-01-22 11:24:56 +0700

    Update parameter for Timer 6 in MX_TIM6_Init

--- BL_Driver/timer.c
+++ BL_Driver/timer.c
@@ -5,18 +5,19 @@ TIM_HandleTypeDef htim6;
 /* TIM6 init function */
 void MX_TIM6_Init(void)
 {
-
+//	In our case, for 1 hz PWM_frequency, set Period to
+//	TIM_Period = 168000000 / 1 - 1 
   TIM_MasterConfigTypeDef sMasterConfig;
-
   htim6.Instance = TIM6;
-  htim6.Init.Prescaler = 0;
+  htim6.Init.Prescaler = 168000;//1 ms
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 0;
+  htim6.Init.Period = 499; //500 ms
   HAL_TIM_Base_Init(&htim6);
 
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig);
+	HAL_TIM_Base_Start_IT(&htim6);
 
 }
 
@@ -26,10 +27,16 @@ void MX_TIM6_Init(void)
 void TIM6_DAC_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
-
+	
   /* USER CODE END TIM6_DAC_IRQn 0 */
   HAL_TIM_IRQHandler(&htim6);
   /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
-
+	HAL_TIM_Base_Start_IT(&htim6);
   /* USER CODE END TIM6_DAC_IRQn 1 */
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	if(htim->Instance==TIM6)
+		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+	
 }
