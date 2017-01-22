94fb097b604ea98cb8154c70a12fb13cd11eba4d
chibinh8 <chibinh8@gmail.com>
2017-01-22 11:06:50 +0700

    Update to filter sensor value and provide Reading API

--- BL_Driver/adc.c
+++ BL_Driver/adc.c
@@ -1,20 +1,42 @@
 #include "adc.h"
+#include "string.h"
+#include "BL_Define.h"
+
+#define NumofSampling 4
+#define NumofSensor (uint8_t)(NumOfSensor1+NumOfSensor2)
+#define NumOfSensor1 7
+#define NumOfSensor2 1
 
 ADC_HandleTypeDef hadc1;
 ADC_HandleTypeDef hadc2;
 
+//ring buffer for each sensor sor will be allocated to make signal smooth
+//there are 8 sensors
+
+uint16_t ringbuff[NumofSensor][NumofSampling] = {0};
+uint16_t FilteredSensorVal[NumofSensor]={0};
+
+static uint8_t IsFilterDone = FALSE;
+
 static void MX_ADC1_Init(void);
 static void MX_ADC2_Init(void);
-
+/*Configure table of sensor channel which s mapping with ADC channel*/
 const uint8_t SensorChannelADC1tbl[NumOfSensor1] = {0};
 const uint8_t SensorChannelADC2tbl[NumOfSensor2] = {0};
-	
+static uint8_t EleBuffIndex = 0;
+
+static void InitRingbuffsensor(void){
+		memset((uint16_t*)ringbuff, 0, (uint8_t)(NumofSampling*NumofSensor));
+		memset((uint16_t*)FilteredSensorVal, 0, NumofSensor);
+}
+
 void BL_ADCInit(void){
 	
 	MX_ADC1_Init(); //configure ADC 1,2 
 	MX_ADC2_Init();
 	HAL_ADC_Start(&hadc1);
 	HAL_ADC_Start(&hadc2);
+	InitRingbuffsensor();
 }
 
 
@@ -98,17 +120,41 @@ void ReadSensor(volatile uint16_t* outsensorval, ADC_HandleTypeDef *hadc, uint8_
 	 *outsensorval = (uint16_t)(tempsensor&0x0FFF);	 
 }
 
+/*Run in task 10 ms*/
 
-void ReadAllSensorfromLine(uint16_t *AllsensorRawVal){
-	
+void ReadAllRawSensorfromLine(void){
+	uint16_t SensorRawVal;
+	uint16_t total_t=0;
 	for(int i=0; i<NumOfSensor1; i++){
-			ReadSensor(AllsensorRawVal,&hadc1,SensorChannelADC1tbl[i]);
-			AllsensorRawVal++; //make sure pass an static array to function
+			ReadSensor(&SensorRawVal,&hadc1,SensorChannelADC1tbl[i]);
+			/*update sensor val for ring buff at index EleBuffIndex*/
+			ringbuff[i][EleBuffIndex] = SensorRawVal;
+			for(int j=0; i<NumofSampling;j++)
+					total_t += ringbuff[i][j];
+			FilteredSensorVal[i] = total_t/NumofSampling;		
 	}	
 	for(int i=0; i<NumOfSensor2; i++){
-			ReadSensor(AllsensorRawVal,&hadc2,SensorChannelADC2tbl[i]);
-			AllsensorRawVal++; //make sure pass an static array to function
+			ReadSensor(&SensorRawVal,&hadc2,SensorChannelADC2tbl[i]);
+			/*update sensor val for ring buff at index EleBuffIndex*/
+			ringbuff[NumOfSensor1+i][EleBuffIndex] = SensorRawVal;
+			for(int j=0; i<NumofSampling;j++)
+					total_t += ringbuff[NumOfSensor1+i][j];
+			FilteredSensorVal[NumOfSensor1+i] = total_t/NumofSampling;					
 	}	
-	/*Noise filter technique*/
 	
+	if(EleBuffIndex==NumofSampling) {		
+		IsFilterDone = TRUE; //reading is ok only if it's already sampled 4 times
+		EleBuffIndex = 0; //reset buffer index
+	}else{
+		EleBuffIndex++;
+	}
+}
+
+/*piblic API to get final val for all sensor and reading status*/
+
+uint8_t ReadAllFinalSensorfromLine(uint16_t *AllsensorFinalVal){
+	
+	if(IsFilterDone==TRUE) AllsensorFinalVal = FilteredSensorVal;
+	
+	return IsFilterDone;	
 }
