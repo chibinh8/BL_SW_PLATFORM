ca069c01ccf2a27e6b480427a1b53387485cd37e
chibinh8 <chibinh8@gmail.com>
2017-01-22 11:07:52 +0700

    Update adc.h

--- BL_Driver/adc.h
+++ BL_Driver/adc.h
@@ -3,8 +3,12 @@
 #define STM32F4XX_HAL
 #include "stm32f4xx_hal.h"
 #endif 
-#define NumOfSensor1 7
-#define NumOfSensor2 1
+
 
 void BL_ADCInit(void);
+
 void ReadSensor(volatile uint16_t* outsensorval, ADC_HandleTypeDef *hadc, uint8_t channel);
+
+uint8_t ReadAllFinalSensorfromLine(uint16_t *AllsensorFinalVal);
+
+void ReadAllRawSensorfromLine(void);
