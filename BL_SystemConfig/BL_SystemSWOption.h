8fa8a46aa3f887d5232a3423973f8aa8e82d7fcd
binhle <chibinh8@gmail.com>
2017-05-02 18:45:02 +0700

    Sync_2_5: Correct API handling for saving ADC thres to NVM

--- BL_SystemConfig/BL_SystemSWOption.h
+++ BL_SystemConfig/BL_SystemSWOption.h
@@ -4,6 +4,14 @@
 /*Timer 6 compiler switch*/
 #undef BL_TIM6
 #undef USEADC2
+#undef DEBUG_ESP
+#undef DEBUG
+#undef UARTDEBUG
+
+#define USE_HAL_DRIVER
+#define STM32F407xx
+#define HAL_PWM_MODULE_ENABLED
+
 
 #endif 
 
